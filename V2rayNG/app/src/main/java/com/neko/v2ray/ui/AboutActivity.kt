2dust/v2rayNG

Optimize and improve toast
Migrate from https://github.com/PureWriter/ToastCompat to https://github.com/GrenderG/Toasty

V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
@@ -14,6 +14,8 @@ import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityAboutBinding
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastError
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.ZipUtil
@@ -50,9 +52,9 @@ class AboutActivity : BaseActivity() {
         binding.layoutBackup.setOnClickListener {
             val ret = backupConfiguration(extDir.absolutePath)
             if (ret.first) {
-                toast(R.string.toast_success)
+                toastSuccess(R.string.toast_success)
             } else {
-                toast(R.string.toast_failure)
+                toastError(R.string.toast_failure)
             }
         }
 
@@ -72,7 +74,7 @@ class AboutActivity : BaseActivity() {
                     )
                 )
             } else {
-                toast(R.string.toast_failure)
+                toastError(R.string.toast_failure)
             }
         }
 
@@ -185,13 +187,13 @@ class AboutActivity : BaseActivity() {
                         }
                     }
                     if (restoreConfiguration(targetFile)) {
-                        toast(R.string.toast_success)
+                        toastSuccess(R.string.toast_success)
                     } else {
-                        toast(R.string.toast_failure)
+                        toastError(R.string.toast_failure)
                     }
                 } catch (e: Exception) {
                     Log.e(AppConfig.ANG_PACKAGE, "Error during file restore: ${e.message}", e)
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 }
             }
         }

V2rayNG/app/src/main/java/com/v2ray/ang/ui/LogcatActivity.kt
@@ -12,6 +12,7 @@ import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityLogcatBinding
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.util.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -119,7 +120,7 @@ class LogcatActivity : BaseActivity(), SwipeRefreshLayout.OnRefreshListener {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.copy_all -> {
             Utils.setClipboard(this, logsets.joinToString("\n"))
-            toast(R.string.toast_success)
+            toastSuccess(R.string.toast_success)
             true
         }
 

V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainActivity.kt
@@ -32,6 +32,7 @@ import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityMainBinding
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastError
 import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MigrateManager
 import com.v2ray.ang.handler.MmkvManager
@@ -505,13 +506,13 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                         }
 
                         countSub > 0 -> initGroupTab()
-                        else -> toast(R.string.toast_failure)
+                        else -> toastError(R.string.toast_failure)
                     }
                     binding.pbWaiting.hide()
                 }
             } catch (e: Exception) {
                 withContext(Dispatchers.Main) {
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                     binding.pbWaiting.hide()
                 }
                 e.printStackTrace()
@@ -615,7 +616,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                     toast(getString(R.string.title_update_config_count, count))
                     mainViewModel.reloadServerList()
                 } else {
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 }
                 binding.pbWaiting.hide()
             }
@@ -631,7 +632,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                 if (ret > 0)
                     toast(getString(R.string.title_export_config_count, ret))
                 else
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 binding.pbWaiting.hide()
             }
         }
@@ -761,9 +762,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 //            }
 //            if (mainViewModel.appendCustomConfigServer(server)) {
 //                mainViewModel.reloadServerList()
-//                toast(R.string.toast_success)
+//                toastSuccess(R.string.toast_success)
 //            } else {
-//                toast(R.string.toast_failure)
+//                toastError(R.string.toast_failure)
 //            }
 //            //adapter.notifyItemInserted(mainViewModel.serverList.lastIndex)
 //        } catch (e: Exception) {

V2rayNG/app/src/main/java/com/v2ray/ang/ui/MainRecyclerAdapter.kt
@@ -19,6 +19,8 @@ import com.v2ray.ang.databinding.ItemRecyclerMainBinding
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastError
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.helper.ItemTouchHelperAdapter
@@ -206,9 +208,9 @@ class MainRecyclerAdapter(val activity: MainActivity) : RecyclerView.Adapter<Mai
      */
     private fun share2Clipboard(guid: String) {
         if (AngConfigManager.share2Clipboard(mActivity, guid) == 0) {
-            mActivity.toast(R.string.toast_success)
+            mActivity.toastSuccess(R.string.toast_success)
         } else {
-            mActivity.toast(R.string.toast_failure)
+            mActivity.toastError(R.string.toast_failure)
         }
     }
 
@@ -218,9 +220,9 @@ class MainRecyclerAdapter(val activity: MainActivity) : RecyclerView.Adapter<Mai
      */
     private fun shareFullContent(guid: String) {
         if (AngConfigManager.shareFullContent2Clipboard(mActivity, guid) == 0) {
-            mActivity.toast(R.string.toast_success)
+            mActivity.toastSuccess(R.string.toast_success)
         } else {
-            mActivity.toast(R.string.toast_failure)
+            mActivity.toastError(R.string.toast_failure)
         }
     }
 

V2rayNG/app/src/main/java/com/v2ray/ang/ui/RoutingEditActivity.kt
@@ -9,6 +9,7 @@ import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityRoutingEditBinding
 import com.v2ray.ang.dto.RulesetItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.SettingsManager
 import com.v2ray.ang.util.Utils
 import kotlinx.coroutines.Dispatchers
@@ -78,7 +79,7 @@ class RoutingEditActivity : BaseActivity() {
         }
 
         SettingsManager.saveRoutingRuleset(position, rulesetItem)
-        toast(R.string.toast_success)
+        toastSuccess(R.string.toast_success)
         finish()
         return true
     }

V2rayNG/app/src/main/java/com/v2ray/ang/ui/ServerCustomConfigActivity.kt
@@ -12,6 +12,7 @@ import com.v2ray.ang.databinding.ActivityServerCustomConfigBinding
 import com.v2ray.ang.dto.EConfigType
 import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.fmt.CustomFmt
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.util.Utils
@@ -89,7 +90,7 @@ class ServerCustomConfigActivity : BaseActivity() {
 
         MmkvManager.encodeServerConfig(editGuid, config)
         MmkvManager.encodeServerRaw(editGuid, binding.editor.text.toString())
-        toast(R.string.toast_success)
+        toastSuccess(R.string.toast_success)
         finish()
         return true
     }

V2rayNG/app/src/main/java/com/v2ray/ang/ui/SubSettingActivity.kt
@@ -12,6 +12,8 @@ import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivitySubSettingBinding
 import com.v2ray.ang.dto.SubscriptionItem
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastError
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.helper.SimpleItemTouchHelperCallback
@@ -65,9 +67,9 @@ class SubSettingActivity : BaseActivity() {
                 delay(500L)
                 launch(Dispatchers.Main) {
                     if (count > 0) {
-                        toast(R.string.toast_success)
+                        toastSuccess(R.string.toast_success)
                     } else {
-                        toast(R.string.toast_failure)
+                        toastError(R.string.toast_failure)
                     }
                     binding.pbWaiting.hide()
                 }

V2rayNG/app/src/main/java/com/v2ray/ang/viewmodel/MainViewModel.kt
@@ -19,6 +19,8 @@ import com.v2ray.ang.dto.ProfileItem
 import com.v2ray.ang.dto.ServersCache
 import com.v2ray.ang.extension.serializable
 import com.v2ray.ang.extension.toast
+import com.v2ray.ang.extension.toastError
+import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.AngConfigManager
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.handler.SettingsManager
@@ -419,12 +421,12 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 }
 
                 AppConfig.MSG_STATE_START_SUCCESS -> {
-                    getApplication<AngApplication>().toast(R.string.toast_services_success)
+                    getApplication<AngApplication>().toastSuccess(R.string.toast_services_success)
                     isRunning.value = true
                 }
 
                 AppConfig.MSG_STATE_START_FAILURE -> {
-                    getApplication<AngApplication>().toast(R.string.toast_services_failure)
+                    getApplication<AngApplication>().toastError(R.string.toast_services_failure)
                     isRunning.value = false
                 }
 

yixuan66/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/ui/AboutActivity.kt
@@ -14,6 +14,8 @@ import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityAboutBinding
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastError
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.ZipUtil
@@ -50,9 +52,9 @@ class AboutActivity : BaseActivity() {
         binding.layoutBackup.setOnClickListener {
             val ret = backupConfiguration(extDir.absolutePath)
             if (ret.first) {
-                toast(R.string.toast_success)
+                toastSuccess(R.string.toast_success)
             } else {
-                toast(R.string.toast_failure)
+                toastError(R.string.toast_failure)
             }
         }
 
@@ -72,7 +74,7 @@ class AboutActivity : BaseActivity() {
                     )
                 )
             } else {
-                toast(R.string.toast_failure)
+                toastError(R.string.toast_failure)
             }
         }
 
@@ -175,13 +177,13 @@ class AboutActivity : BaseActivity() {
                         }
                     }
                     if (restoreConfiguration(targetFile)) {
-                        toast(R.string.toast_success)
+                        toastSuccess(R.string.toast_success)
                     } else {
-                        toast(R.string.toast_failure)
+                        toastError(R.string.toast_failure)
                     }
                 } catch (e: Exception) {
                     Log.e(AppConfig.ANG_PACKAGE, "Error during file restore: ${e.message}", e)
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 }
             }
         }

V2rayNG/app/src/main/java/com/neko/v2ray/ui/LogcatActivity.kt
@@ -14,6 +14,7 @@ import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityLogcatBinding
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.util.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -123,7 +124,7 @@ class LogcatActivity : BaseActivity(), SwipeRefreshLayout.OnRefreshListener {
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.copy_all -> {
             Utils.setClipboard(this, logsets.joinToString("\n"))
-            toast(R.string.toast_success)
+            toastSuccess(R.string.toast_success)
             true
         }
 

V2rayNG/app/src/main/java/com/neko/v2ray/ui/MainActivity.kt
@@ -35,6 +35,7 @@ import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityMainBinding
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastError
 import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MigrateManager
 import com.neko.v2ray.handler.MmkvManager
@@ -628,13 +629,13 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                         }
 
                         countSub > 0 -> initGroupTab()
-                        else -> toast(R.string.toast_failure)
+                        else -> toastError(R.string.toast_failure)
                     }
                     binding.pbWaiting.hide()
                 }
             } catch (e: Exception) {
                 withContext(Dispatchers.Main) {
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                     binding.pbWaiting.hide()
                 }
                 e.printStackTrace()
@@ -738,7 +739,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                     toast(getString(R.string.title_update_config_count, count))
                     mainViewModel.reloadServerList()
                 } else {
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 }
                 binding.pbWaiting.hide()
             }
@@ -754,7 +755,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                 if (ret > 0)
                     toast(getString(R.string.title_export_config_count, ret))
                 else
-                    toast(R.string.toast_failure)
+                    toastError(R.string.toast_failure)
                 binding.pbWaiting.hide()
             }
         }
@@ -905,9 +906,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 //            }
 //            if (mainViewModel.appendCustomConfigServer(server)) {
 //                mainViewModel.reloadServerList()
-//                toast(R.string.toast_success)
+//                toastSuccess(R.string.toast_success)
 //            } else {
-//                toast(R.string.toast_failure)
+//                toastError(R.string.toast_failure)
 //            }
 //            //adapter.notifyItemInserted(mainViewModel.serverList.lastIndex)
 //        } catch (e: Exception) {

V2rayNG/app/src/main/java/com/neko/v2ray/ui/MainRecyclerAdapter.kt
@@ -19,6 +19,8 @@ import com.neko.v2ray.databinding.ItemRecyclerMainBinding
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastError
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.helper.ItemTouchHelperAdapter
@@ -206,9 +208,9 @@ class MainRecyclerAdapter(val activity: MainActivity) : RecyclerView.Adapter<Mai
      */
     private fun share2Clipboard(guid: String) {
         if (AngConfigManager.share2Clipboard(mActivity, guid) == 0) {
-            mActivity.toast(R.string.toast_success)
+            mActivity.toastSuccess(R.string.toast_success)
         } else {
-            mActivity.toast(R.string.toast_failure)
+            mActivity.toastError(R.string.toast_failure)
         }
     }
 
@@ -218,9 +220,9 @@ class MainRecyclerAdapter(val activity: MainActivity) : RecyclerView.Adapter<Mai
      */
     private fun shareFullContent(guid: String) {
         if (AngConfigManager.shareFullContent2Clipboard(mActivity, guid) == 0) {
-            mActivity.toast(R.string.toast_success)
+            mActivity.toastSuccess(R.string.toast_success)
         } else {
-            mActivity.toast(R.string.toast_failure)
+            mActivity.toastError(R.string.toast_failure)
         }
     }
 

V2rayNG/app/src/main/java/com/neko/v2ray/ui/RoutingEditActivity.kt
@@ -5,18 +5,18 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
-import com.google.android.material.appbar.MaterialToolbar
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityRoutingEditBinding
 import com.neko.v2ray.dto.RulesetItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.SettingsManager
-import com.neko.v2ray.util.SoftInputAssist
 import com.neko.v2ray.util.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.neko.imageslider.ImageSlider
 import com.neko.imageslider.constants.ActionTypes
 import com.neko.imageslider.constants.AnimationTypes
@@ -131,7 +131,7 @@ class RoutingEditActivity : BaseActivity() {
         }
 
         SettingsManager.saveRoutingRuleset(position, rulesetItem)
-        toast(R.string.toast_success)
+        toastSuccess(R.string.toast_success)
         finish()
         return true
     }

V2rayNG/app/src/main/java/com/neko/v2ray/ui/ServerCustomConfigActivity.kt
@@ -14,6 +14,7 @@ import com.neko.v2ray.databinding.ActivityServerCustomConfigBinding
 import com.neko.v2ray.dto.EConfigType
 import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.fmt.CustomFmt
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.util.Utils
@@ -146,7 +147,7 @@ class ServerCustomConfigActivity : BaseActivity() {
 
         MmkvManager.encodeServerConfig(editGuid, config)
         MmkvManager.encodeServerRaw(editGuid, binding.editor.text.toString())
-        toast(R.string.toast_success)
+        toastSuccess(R.string.toast_success)
         finish()
         return true
     }

V2rayNG/app/src/main/java/com/neko/v2ray/ui/SubSettingActivity.kt
@@ -14,6 +14,8 @@ import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivitySubSettingBinding
 import com.neko.v2ray.dto.SubscriptionItem
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastError
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.helper.SimpleItemTouchHelperCallback
@@ -85,9 +87,9 @@ class SubSettingActivity : BaseActivity() {
                 delay(500L)
                 launch(Dispatchers.Main) {
                     if (count > 0) {
-                        toast(R.string.toast_success)
+                        toastSuccess(R.string.toast_success)
                     } else {
-                        toast(R.string.toast_failure)
+                        toastError(R.string.toast_failure)
                     }
                     binding.pbWaiting.hide()
                 }

V2rayNG/app/src/main/java/com/neko/v2ray/viewmodel/MainViewModel.kt
@@ -19,6 +19,8 @@ import com.neko.v2ray.dto.ProfileItem
 import com.neko.v2ray.dto.ServersCache
 import com.neko.v2ray.extension.serializable
 import com.neko.v2ray.extension.toast
+import com.neko.v2ray.extension.toastError
+import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.AngConfigManager
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.handler.SettingsManager
@@ -419,12 +421,12 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 }
 
                 AppConfig.MSG_STATE_START_SUCCESS -> {
-                    getApplication<AngApplication>().toast(R.string.toast_services_success)
+                    getApplication<AngApplication>().toastSuccess(R.string.toast_services_success)
                     isRunning.value = true
                 }
 
                 AppConfig.MSG_STATE_START_FAILURE -> {
-                    getApplication<AngApplication>().toast(R.string.toast_services_failure)
+                    getApplication<AngApplication>().toastError(R.string.toast_services_failure)
                     isRunning.value = false
                 }
 