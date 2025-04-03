2dust/v2rayNG

Added check for updates

V2rayNG/app/src/main/java/com/v2ray/ang/dto/CheckUpdateResult.kt
@@ -1,0 +1,10 @@
+package com.v2ray.ang.dto
+
+data class CheckUpdateResult(
+    val hasUpdate: Boolean,
+    val latestVersion: String? = null,
+    val releaseNotes: String? = null,
+    val downloadUrl: String? = null,
+    val error: String? = null,
+    val isPreRelease: Boolean = false
+)

V2rayNG/app/src/main/java/com/v2ray/ang/dto/GitHubRelease.kt
@@ -1,0 +1,23 @@
+package com.v2ray.ang.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class GitHubRelease(
+    @SerializedName("tag_name")
+    val tagName: String,
+    @SerializedName("body")
+    val body: String,
+    @SerializedName("assets")
+    val assets: List<Asset>,
+    @SerializedName("prerelease")
+    val prerelease: Boolean = false,
+    @SerializedName("published_at")
+    val publishedAt: String = ""
+) {
+    data class Asset(
+        @SerializedName("name")
+        val name: String,
+        @SerializedName("browser_download_url")
+        val browserDownloadUrl: String
+    )
+}

V2rayNG/app/src/main/java/com/v2ray/ang/handler/UpdateCheckerManager.kt
@@ -1,0 +1,112 @@
+package com.v2ray.ang.handler
+
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import com.v2ray.ang.AppConfig
+import com.v2ray.ang.BuildConfig
+import com.v2ray.ang.dto.CheckUpdateResult
+import com.v2ray.ang.dto.GitHubRelease
+import com.v2ray.ang.util.HttpUtil
+import com.v2ray.ang.util.JsonUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
+object UpdateCheckerManager {
+    suspend fun checkForUpdate(includePreRelease: Boolean = false): CheckUpdateResult = withContext(Dispatchers.IO) {
+        try {
+            val url = if (includePreRelease) {
+                AppConfig.v2rayNGAPIUrl
+            } else {
+                "${AppConfig.v2rayNGAPIUrl}/latest"
+            }
+
+            var response = HttpUtil.getUrlContent(url, 5000)
+            if (response.isNullOrEmpty()) {
+                val httpPort = SettingsManager.getHttpPort()
+                response = HttpUtil.getUrlContent(url, 5000, httpPort) ?: throw IllegalStateException("Failed to get response")
+            }
+
+            val latestRelease = if (includePreRelease) {
+                JsonUtil.fromJson(response, Array<GitHubRelease>::class.java)
+                    .filter { it.prerelease }
+                    .maxByOrNull { it.publishedAt }
+                    ?: throw IllegalStateException("No pre-release found")
+            } else {
+                JsonUtil.fromJson(response, GitHubRelease::class.java)
+            }
+
+            val latestVersion = latestRelease.tagName.removePrefix("v")
+            Log.i(AppConfig.TAG, "Found new version: $latestVersion (current: ${BuildConfig.VERSION_NAME})")
+
+            return@withContext if (compareVersions(latestVersion, BuildConfig.VERSION_NAME) > 0) {
+                val downloadUrl = getDownloadUrl(latestRelease, Build.SUPPORTED_ABIS[0])
+                CheckUpdateResult(
+                    hasUpdate = true,
+                    latestVersion = latestVersion,
+                    releaseNotes = latestRelease.body,
+                    downloadUrl = downloadUrl,
+                    isPreRelease = latestRelease.prerelease
+                )
+            } else {
+                CheckUpdateResult(hasUpdate = false)
+            }
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to check for updates: ${e.message}")
+            return@withContext CheckUpdateResult(hasUpdate = false, error = e.message)
+        }
+    }
+
+    suspend fun downloadApk(context: Context, downloadUrl: String): File? = withContext(Dispatchers.IO) {
+        try {
+            val httpPort = SettingsManager.getHttpPort()
+            val connection = HttpUtil.createProxyConnection(downloadUrl, httpPort, 10000, 10000, true)
+                ?: throw IllegalStateException("Failed to create connection")
+
+            try {
+                val apkFile = File(context.cacheDir, "update.apk")
+                Log.i(AppConfig.TAG, "Downloading APK to: ${apkFile.absolutePath}")
+
+                FileOutputStream(apkFile).use { outputStream ->
+                    connection.inputStream.use { inputStream ->
+                        inputStream.copyTo(outputStream)
+                    }
+                }
+                Log.i(AppConfig.TAG, "APK download completed")
+                return@withContext apkFile
+            } catch (e: Exception) {
+                Log.e(AppConfig.TAG, "Failed to download APK: ${e.message}")
+                return@withContext null
+            } finally {
+                try {
+                    connection.disconnect()
+                } catch (e: Exception) {
+                    Log.e(AppConfig.TAG, "Error closing connection: ${e.message}")
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to initiate download: ${e.message}")
+            return@withContext null
+        }
+    }
+
+    private fun compareVersions(version1: String, version2: String): Int {
+        val v1 = version1.split(".")
+        val v2 = version2.split(".")
+
+        for (i in 0 until maxOf(v1.size, v2.size)) {
+            val num1 = if (i < v1.size) v1[i].toInt() else 0
+            val num2 = if (i < v2.size) v2[i].toInt() else 0
+            if (num1 != num2) return num1 - num2
+        }
+        return 0
+    }
+
+    private fun getDownloadUrl(release: GitHubRelease, abi: String): String {
+        return release.assets.find { it.name.contains(abi) }?.browserDownloadUrl
+            ?: release.assets.firstOrNull()?.browserDownloadUrl
+            ?: throw IllegalStateException("No compatible APK found")
+    }
+}

V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
@@ -6,19 +6,25 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
 import com.tencent.mmkv.MMKV
 import com.v2ray.ang.AppConfig
 import com.v2ray.ang.BuildConfig
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ActivityAboutBinding
+import com.v2ray.ang.dto.CheckUpdateResult
 import com.v2ray.ang.extension.toast
 import com.v2ray.ang.extension.toastError
 import com.v2ray.ang.extension.toastSuccess
+import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.handler.SpeedtestManager
+import com.v2ray.ang.handler.UpdateCheckerManager
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.ZipUtil
+import kotlinx.coroutines.launch
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -96,6 +102,15 @@ class AboutActivity : BaseActivity() {
                 requestPermissionLauncher.launch(permission)
             }
         }
+
+        binding.layoutCheckUpdate.setOnClickListener {
+            checkForUpdates(binding.checkPreRelease.isChecked)
+        }
+
+        binding.checkPreRelease.setOnCheckedChangeListener { _, isChecked ->
+            MmkvManager.encodeSettings(AppConfig.PREF_CHECK_UPDATE_PRE_RELEASE, isChecked)
+        }
+        binding.checkPreRelease.isChecked = MmkvManager.decodeSettingsBool(AppConfig.PREF_CHECK_UPDATE_PRE_RELEASE, false)
 
         binding.layoutSoureCcode.setOnClickListener {
             Utils.openUri(this, AppConfig.v2rayNGUrl)
@@ -197,4 +212,28 @@ class AboutActivity : BaseActivity() {
                 }
             }
         }
+
+    private fun checkForUpdates(includePreRelease: Boolean) {
+        lifecycleScope.launch {
+            val result = UpdateCheckerManager.checkForUpdate(includePreRelease)
+            if (result.hasUpdate) {
+                showUpdateDialog(result)
+            } else {
+                toast(R.string.update_already_latest_version)
+            }
+        }
+    }
+
+    private fun showUpdateDialog(result: CheckUpdateResult) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.update_new_version_found, result.latestVersion))
+            .setMessage(result.releaseNotes)
+            .setPositiveButton(R.string.update_now) { _, _ ->
+                result.downloadUrl?.let {
+                    Utils.openUri(this, it)
+                }
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
+    }
 }

yixuan66/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/dto/CheckUpdateResult.kt
@@ -1,0 +1,10 @@
+package com.neko.v2ray.dto
+
+data class CheckUpdateResult(
+    val hasUpdate: Boolean,
+    val latestVersion: String? = null,
+    val releaseNotes: String? = null,
+    val downloadUrl: String? = null,
+    val error: String? = null,
+    val isPreRelease: Boolean = false
+)

V2rayNG/app/src/main/java/com/neko/v2ray/dto/GitHubRelease.kt
@@ -1,0 +1,23 @@
+package com.neko.v2ray.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class GitHubRelease(
+    @SerializedName("tag_name")
+    val tagName: String,
+    @SerializedName("body")
+    val body: String,
+    @SerializedName("assets")
+    val assets: List<Asset>,
+    @SerializedName("prerelease")
+    val prerelease: Boolean = false,
+    @SerializedName("published_at")
+    val publishedAt: String = ""
+) {
+    data class Asset(
+        @SerializedName("name")
+        val name: String,
+        @SerializedName("browser_download_url")
+        val browserDownloadUrl: String
+    )
+}

V2rayNG/app/src/main/java/com/neko/v2ray/handler/UpdateCheckerManager.kt
@@ -1,0 +1,112 @@
+package com.neko.v2ray.handler
+
+import android.content.Context
+import android.os.Build
+import android.util.Log
+import com.neko.v2ray.AppConfig
+import com.neko.v2ray.BuildConfig
+import com.neko.v2ray.dto.CheckUpdateResult
+import com.neko.v2ray.dto.GitHubRelease
+import com.neko.v2ray.util.HttpUtil
+import com.neko.v2ray.util.JsonUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+
+object UpdateCheckerManager {
+    suspend fun checkForUpdate(includePreRelease: Boolean = false): CheckUpdateResult = withContext(Dispatchers.IO) {
+        try {
+            val url = if (includePreRelease) {
+                AppConfig.v2rayNGAPIUrl
+            } else {
+                "${AppConfig.v2rayNGAPIUrl}/latest"
+            }
+
+            var response = HttpUtil.getUrlContent(url, 5000)
+            if (response.isNullOrEmpty()) {
+                val httpPort = SettingsManager.getHttpPort()
+                response = HttpUtil.getUrlContent(url, 5000, httpPort) ?: throw IllegalStateException("Failed to get response")
+            }
+
+            val latestRelease = if (includePreRelease) {
+                JsonUtil.fromJson(response, Array<GitHubRelease>::class.java)
+                    .filter { it.prerelease }
+                    .maxByOrNull { it.publishedAt }
+                    ?: throw IllegalStateException("No pre-release found")
+            } else {
+                JsonUtil.fromJson(response, GitHubRelease::class.java)
+            }
+
+            val latestVersion = latestRelease.tagName.removePrefix("v")
+            Log.i(AppConfig.TAG, "Found new version: $latestVersion (current: ${BuildConfig.VERSION_NAME})")
+
+            return@withContext if (compareVersions(latestVersion, BuildConfig.VERSION_NAME) > 0) {
+                val downloadUrl = getDownloadUrl(latestRelease, Build.SUPPORTED_ABIS[0])
+                CheckUpdateResult(
+                    hasUpdate = true,
+                    latestVersion = latestVersion,
+                    releaseNotes = latestRelease.body,
+                    downloadUrl = downloadUrl,
+                    isPreRelease = latestRelease.prerelease
+                )
+            } else {
+                CheckUpdateResult(hasUpdate = false)
+            }
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to check for updates: ${e.message}")
+            return@withContext CheckUpdateResult(hasUpdate = false, error = e.message)
+        }
+    }
+
+    suspend fun downloadApk(context: Context, downloadUrl: String): File? = withContext(Dispatchers.IO) {
+        try {
+            val httpPort = SettingsManager.getHttpPort()
+            val connection = HttpUtil.createProxyConnection(downloadUrl, httpPort, 10000, 10000, true)
+                ?: throw IllegalStateException("Failed to create connection")
+
+            try {
+                val apkFile = File(context.cacheDir, "update.apk")
+                Log.i(AppConfig.TAG, "Downloading APK to: ${apkFile.absolutePath}")
+
+                FileOutputStream(apkFile).use { outputStream ->
+                    connection.inputStream.use { inputStream ->
+                        inputStream.copyTo(outputStream)
+                    }
+                }
+                Log.i(AppConfig.TAG, "APK download completed")
+                return@withContext apkFile
+            } catch (e: Exception) {
+                Log.e(AppConfig.TAG, "Failed to download APK: ${e.message}")
+                return@withContext null
+            } finally {
+                try {
+                    connection.disconnect()
+                } catch (e: Exception) {
+                    Log.e(AppConfig.TAG, "Error closing connection: ${e.message}")
+                }
+            }
+        } catch (e: Exception) {
+            Log.e(AppConfig.TAG, "Failed to initiate download: ${e.message}")
+            return@withContext null
+        }
+    }
+
+    private fun compareVersions(version1: String, version2: String): Int {
+        val v1 = version1.split(".")
+        val v2 = version2.split(".")
+
+        for (i in 0 until maxOf(v1.size, v2.size)) {
+            val num1 = if (i < v1.size) v1[i].toInt() else 0
+            val num2 = if (i < v2.size) v2[i].toInt() else 0
+            if (num1 != num2) return num1 - num2
+        }
+        return 0
+    }
+
+    private fun getDownloadUrl(release: GitHubRelease, abi: String): String {
+        return release.assets.find { it.name.contains(abi) }?.browserDownloadUrl
+            ?: release.assets.firstOrNull()?.browserDownloadUrl
+            ?: throw IllegalStateException("No compatible APK found")
+    }
+}

V2rayNG/app/src/main/java/com/neko/v2ray/ui/AboutActivity.kt
@@ -6,19 +6,25 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
 import com.tencent.mmkv.MMKV
 import com.neko.v2ray.AppConfig
 import com.neko.v2ray.BuildConfig
 import com.neko.v2ray.R
 import com.neko.v2ray.databinding.ActivityAboutBinding
+import com.neko.v2ray.dto.CheckUpdateResult
 import com.neko.v2ray.extension.toast
 import com.neko.v2ray.extension.toastError
 import com.neko.v2ray.extension.toastSuccess
+import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.handler.SpeedtestManager
+import com.neko.v2ray.handler.UpdateCheckerManager
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.ZipUtil
+import kotlinx.coroutines.launch
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -96,6 +102,15 @@ class AboutActivity : BaseActivity() {
                 requestPermissionLauncher.launch(permission)
             }
         }
+
+        binding.layoutCheckUpdate.setOnClickListener {
+            checkForUpdates(binding.checkPreRelease.isChecked)
+        }
+
+        binding.checkPreRelease.setOnCheckedChangeListener { _, isChecked ->
+            MmkvManager.encodeSettings(AppConfig.PREF_CHECK_UPDATE_PRE_RELEASE, isChecked)
+        }
+        binding.checkPreRelease.isChecked = MmkvManager.decodeSettingsBool(AppConfig.PREF_CHECK_UPDATE_PRE_RELEASE, false)
 
         binding.layoutSoureCcode.setOnClickListener {
             Utils.openUri(this, AppConfig.v2rayNGUrl)
@@ -187,4 +202,28 @@ class AboutActivity : BaseActivity() {
                 }
             }
         }
+
+    private fun checkForUpdates(includePreRelease: Boolean) {
+        lifecycleScope.launch {
+            val result = UpdateCheckerManager.checkForUpdate(includePreRelease)
+            if (result.hasUpdate) {
+                showUpdateDialog(result)
+            } else {
+                toast(R.string.update_already_latest_version)
+            }
+        }
+    }
+
+    private fun showUpdateDialog(result: CheckUpdateResult) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.update_new_version_found, result.latestVersion))
+            .setMessage(result.releaseNotes)
+            .setPositiveButton(R.string.update_now) { _, _ ->
+                result.downloadUrl?.let {
+                    Utils.openUri(this, it)
+                }
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
+    }
 }