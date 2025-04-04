2dust/v2rayNG

If it is the Google Play version, the update check will not be displayed within 2 days after update.

V2rayNG/app/src/main/java/com/v2ray/ang/ui/AboutActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -22,6 +23,7 @@ import com.v2ray.ang.extension.toastSuccess
 import com.v2ray.ang.handler.MmkvManager
 import com.v2ray.ang.handler.SpeedtestManager
 import com.v2ray.ang.handler.UpdateCheckerManager
+import com.v2ray.ang.util.AppManagerUtil
 import com.v2ray.ang.util.Utils
 import com.v2ray.ang.util.ZipUtil
 import kotlinx.coroutines.launch
@@ -103,6 +105,14 @@ class AboutActivity : BaseActivity() {
             }
         }
 
+        //If it is the Google Play version, not be displayed within 2 days after update
+        if (Utils.isGoogleFlavor()) {
+            val lastUpdateTime = AppManagerUtil.getLastUpdateTime(this)
+            val currentTime = System.currentTimeMillis()
+            if ((currentTime - lastUpdateTime) < 2 * 24 * 60 * 60 * 1000L) {
+                binding.layoutCheckUpdate.visibility = View.GONE
+            }
+        }
         binding.layoutCheckUpdate.setOnClickListener {
             checkForUpdates(binding.checkPreRelease.isChecked)
         }

V2rayNG/app/src/main/java/com/v2ray/ang/util/Utils.kt
@@ -17,8 +17,8 @@ import android.webkit.URLUtil
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.v2ray.ang.AppConfig
-import com.v2ray.ang.AppConfig.ANG_PACKAGE
 import com.v2ray.ang.AppConfig.LOOPBACK
+import com.v2ray.ang.BuildConfig
 import java.io.IOException
 import java.net.ServerSocket
 import java.net.URLDecoder
@@ -486,7 +486,14 @@ object Utils {
      *
      * @return True if the package is Xray, false otherwise.
      */
-    fun isXray(): Boolean = ANG_PACKAGE.startsWith("com.v2ray.ang")
+    fun isXray(): Boolean = BuildConfig.APPLICATION_ID.startsWith("com.v2ray.ang")
+
+    /**
+     * Check if it is the Google Play version.
+     *
+     * @return True if the package is Google Play, false otherwise.
+     */
+    fun isGoogleFlavor(): Boolean = BuildConfig.FLAVOR == "playstore"
 
 }
 

yixuan66/Neko-ray

V2rayNG/app/src/main/java/com/neko/v2ray/ui/AboutActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
@@ -22,6 +23,7 @@ import com.neko.v2ray.extension.toastSuccess
 import com.neko.v2ray.handler.MmkvManager
 import com.neko.v2ray.handler.SpeedtestManager
 import com.neko.v2ray.handler.UpdateCheckerManager
+import com.neko.v2ray.util.AppManagerUtil
 import com.neko.v2ray.util.Utils
 import com.neko.v2ray.util.ZipUtil
 import kotlinx.coroutines.launch
@@ -103,6 +105,14 @@ class AboutActivity : BaseActivity() {
             }
         }
 
+        //If it is the Google Play version, not be displayed within 2 days after update
+        // if (Utils.isGoogleFlavor()) {
+            val lastUpdateTime = AppManagerUtil.getLastUpdateTime(this)
+            val currentTime = System.currentTimeMillis()
+            if ((currentTime - lastUpdateTime) < 2 * 24 * 60 * 60 * 1000L) {
+                binding.layoutCheckUpdate.visibility = View.GONE
+            }
+        // }
         binding.layoutCheckUpdate.setOnClickListener {
             checkForUpdates(binding.checkPreRelease.isChecked)
         }

V2rayNG/app/src/main/java/com/neko/v2ray/util/Utils.kt
@@ -17,8 +17,8 @@ import android.webkit.URLUtil
 import androidx.core.content.ContextCompat
 import androidx.core.net.toUri
 import com.neko.v2ray.AppConfig
-import com.neko.v2ray.AppConfig.ANG_PACKAGE
 import com.neko.v2ray.AppConfig.LOOPBACK
+import com.neko.v2ray.BuildConfig
 import java.io.IOException
 import java.net.ServerSocket
 import java.net.URLDecoder
@@ -486,7 +486,14 @@ object Utils {
      *
      * @return True if the package is Xray, false otherwise.
      */
-    fun isXray(): Boolean = ANG_PACKAGE.startsWith("com.neko.v2ray")
+    fun isXray(): Boolean = BuildConfig.APPLICATION_ID.startsWith("com.neko.v2ray")
+
+    /**
+     * Check if it is the Google Play version.
+     *
+     * @return True if the package is Google Play, false otherwise.
+     */
+    // fun isGoogleFlavor(): Boolean = BuildConfig.FLAVOR == "playstore"
 
 }
 